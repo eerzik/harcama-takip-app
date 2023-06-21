deb3dc587e12e4bfddaaeab88137d4ff32f5370e
Engin ERZİK <erzik.engin@gmail.com>
2023-06-21 11:10:50 +0300

    Parolayı Göster-Gizle Ayarı

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,21 +1,30 @@
 import './Login.module.css'
-import { Container, Typography, Button, FormControl, FilledInput, InputLabel } from '@mui/material'
+import { Container, Typography, Button, FormControl, FilledInput, InputLabel, InputAdornment, IconButton } from '@mui/material'
 import { useState } from 'react'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 
 export default function Login() {
 
     const [values, setValues] = useState({
         email: '',
-        password: ''
+        password: '',
+        showPassword: false
     })
 
-    const handleChange=(prop)=>(event)=>{
-        setValues({...values,[prop]:event.target.value})
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value })
     }
 
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log(values);
+    }
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword
+        })
     }
 
     return (
@@ -32,7 +41,15 @@ export default function Login() {
 
                 <FormControl fullWidth sx={{ my: 5 }} >
                     <InputLabel htmlFor="password" >Parola</InputLabel>
-                    <FilledInput id="password" value={values.password} onChange={handleChange('password')} label="Parola"  ></FilledInput>
+                    <FilledInput type={values.showPassword ? 'text' : 'password'} autoComplete='off' id="password" value={values.password} onChange={handleChange('password')} label="Parola" endAdornment={
+                            <InputAdornment position='end' >
+                                <IconButton aria-label='Toogle Password' onClick={handleClickShowPassword} edge="end" >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }  >
+
+                    </FilledInput>
                 </FormControl>
                 <Button variant='outlined' type='submit' color='info' size='large' sx={{ mt: 5 }} >
                     GİRİŞ
