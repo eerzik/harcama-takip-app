9ba6ebeebe1cce7add3f7fa4eecf5975f4e1107d
Engin ERZİK <erzik.engin@gmail.com>
2023-06-21 11:28:56 +0300

    Signup Sayfasını Oluşturma

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -1,10 +1,69 @@
+
+
 import './Signup.module.css'
+import { Container, Typography, Button, FormControl, OutlinedInput, InputLabel, InputAdornment, IconButton } from '@mui/material'
+import { useState } from 'react'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 
+export default function Signup() {
 
-export default function Signup () {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        showPassword: false,
+        userName:''
+    })
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(values);
+    }
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword
+        })
+    }
+
     return (
-        <div>
-           Signup
-        </div>
+        <Container>
+            <form onSubmit={handleSubmit} >
+                <Typography sx={{ mt: 15, ml: 5, fontWeight: 'bold' }} variant='h4' color={'darkslateblue'} >
+                   Üye Ol
+                </Typography>
+
+                <FormControl fullWidth sx={{ mt: 5 }} >
+                    <InputLabel htmlFor="email" >Email</InputLabel>
+                    <OutlinedInput id="email" label="Email" value={values.email} onChange={handleChange('email')}  ></OutlinedInput>
+                </FormControl>
+
+                <FormControl fullWidth sx={{ mt: 5 }} >
+                    <InputLabel htmlFor="password" >Parola</InputLabel>
+                    <OutlinedInput type={values.showPassword ? 'text' : 'password'} autoComplete='off' id="password" value={values.password} onChange={handleChange('password')} label="Parola" endAdornment={
+                            <InputAdornment position='end' >
+                                <IconButton aria-label='Toogle Password' onClick={handleClickShowPassword} edge="end" >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }  >
+                    </OutlinedInput>
+                </FormControl>
+
+                <FormControl fullWidth sx={{ my: 5 }} >
+                    <InputLabel htmlFor="user-name" >Kullanıcı Ad</InputLabel>
+                    <OutlinedInput id="user-name" label="Kullanıcı Ad" value={values.userName} onChange={handleChange('userName')}  ></OutlinedInput>
+                </FormControl>
+
+                <Button variant='contained' type='submit' color='info' size='large' sx={{ mt: 5 }} >
+                    Üye Ol
+                </Button>
+            </form>
+        </Container>
     )
 }
+
