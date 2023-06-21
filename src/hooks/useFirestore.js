02aa61eabd8322c6ccff86f084638e3a21884f95
Engin ERZİK <erzik.engin@gmail.com>
2023-06-21 17:20:57 +0300

    Belge Ekleme Fonksiyonunu oluşturma

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useReducer } from "react";
 import { db } from "../firebase/config";
-import { collection } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 
 const baslangicVeri = {
     belge: null,
@@ -11,6 +11,13 @@ const baslangicVeri = {
 
 const firestoreReducer = (state, action) => {
     switch (action.type) {
+        case 'BEKLIYOR':
+            return { hata: null, belge: null, basari: false, bekliyor: true }
+
+        case 'BELGE_EKLENDI':
+            return { hata: null, belge: action.payload, basari: true, bekliyor: false }
+        case 'HATA':
+            return { hata: action.payload, belge: null, basari: false, bekliyor: false }
 
         default:
             return state
@@ -22,18 +29,28 @@ export const useFirestore = (col) => {
     const [response, dispatch] = useReducer(firestoreReducer, baslangicVeri);
     const [iptal, setIptal] = useState(false)
 
-    const ref=collection(db,col)
+    const ref = collection(db, col)
 
-    const belgeEkle=async (belge)=>{
+    const belgeEkle = async (belge) => {
+        dispatch({ type: 'BEKLIYOR' })
+        try {
+            const eklenenBelge = await addDoc(ref, belge)
+            if (!iptal) {
+                dispatch({ type: 'BELGE_EKLENDI', payload: eklenenBelge })
+            }
+        } catch (error) {
+            if (!iptal) {
+                dispatch({ type: 'HATA', payload: error.message })
+            }
+        }
+    }
+
+    const belgeSil = async (id) => {
 
     }
 
-    const belgeSil=async (id)=>{
-        
-    }
-
-    useEffect(()=>{
-        return ()=>setIptal(true)
+    useEffect(() => {
+        return () => setIptal(true)
     })
-    return {belgeEkle,belgeSil,response}
+    return { belgeEkle, belgeSil, response }
 }
